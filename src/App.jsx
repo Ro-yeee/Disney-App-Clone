33a1d201fcf4071cf5b8dd7b1dfa5dc451c6c389
Roy Joseph <royjoseph.mec@gmail.com>
2023-08-10 17:13:14 +0530

    Shows user picture and nav items when logged in.

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,23 @@
+import { useState } from 'react'
 import './App.css'
 import AppRouter from './AppRouter'
 import Header from './components/Header/Header'
 
 function App() {
 
+  const [user,setUser] = useState({name: "",
+                                   email: "",
+                                   photo: ""})
+
+  const setLogin = (data) =>{
+    setUser({name: data.displayName,
+             email: data.email,
+             photo: data.photoURL })
+  }
+
   return (
     <div>
-      <Header/>
+      <Header user={user} setLogin={setLogin}/>
       <AppRouter/>
     </div>
   )
--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,11 +1,15 @@
 import { auth, provider } from '../../FirebaseConfig'
 import './Header.css'
 
-function Header() {
+function Header({user, setLogin}) {
 
     const handleLogin = () =>{
         auth.signInWithPopup(provider)
-            .then(result => console.log(result))
+            .then(result => {
+               const {user} = result 
+               setLogin(user)
+               console.log(user)
+            })
             .catch(error => console.log(error))
     }
 
@@ -14,35 +18,42 @@ function Header() {
         <a href='/'>
             <img className='logo' src='/images/logo.svg'/>
         </a>
-        <div className='navMenu'>
-            <a className='links' href="/">
-                <img src="/images/home-icon.svg" alt="HOME" />
-                <span>HOME</span>
-            </a>
-            <a className='links' href="/">
-                <img src="/images/search-icon.svg" alt="SEARCH" />
-                <span>SEARCH</span>
-            </a>
-            <a className='links' href="/">
-                <img src="/images/watchlist-icon.svg" alt="WATCHLIST" />
-                <span>WATCHLIST</span>
-            </a>
-            <a className='links' href="/">
-                <img src="/images/original-icon.svg" alt="ORIGINALS" />
-                <span>ORIGINALS</span>
-            </a>
-            <a className='links' href="/">
-                <img src="/images/movie-icon.svg" alt="MOVIES" />
-                <span>MOVIES</span>
-            </a>
-            <a className='links' href="/">
-                <img src="/images/tv-icon.svg" alt="TV" />
-                <span>TV</span>
-            </a>
-        </div>
-        <button className='loginBtn' onClick={handleLogin}>
-            LOGIN
-        </button>
+        {
+            user.name === "" ?
+            <button className='loginBtn' onClick={handleLogin}>
+                LOGIN
+            </button>
+            :
+            <>
+            <div className='navMenu'>
+                <a className='links' href="/">
+                    <img src="/images/home-icon.svg" alt="HOME" />
+                    <span>HOME</span>
+                </a>
+                <a className='links' href="/">
+                    <img src="/images/search-icon.svg" alt="SEARCH" />
+                    <span>SEARCH</span>
+                </a>
+                <a className='links' href="/">
+                    <img src="/images/watchlist-icon.svg" alt="WATCHLIST" />
+                    <span>WATCHLIST</span>
+                </a>
+                <a className='links' href="/">
+                    <img src="/images/original-icon.svg" alt="ORIGINALS" />
+                    <span>ORIGINALS</span>
+                </a>
+                <a className='links' href="/">
+                    <img src="/images/movie-icon.svg" alt="MOVIES" />
+                    <span>MOVIES</span>
+                </a>
+                <a className='links' href="/">
+                    <img src="/images/tv-icon.svg" alt="TV" />
+                    <span>TV</span>
+                </a>
+            </div>
+            <img className='userImage'  src={user.photo}/>
+            </>
+        }
     </nav>
   )
 }
