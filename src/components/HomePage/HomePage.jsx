0387c0f56c91f3f8321208017e62963767c5d9c0
Roy Joseph <royjoseph.mec@gmail.com>
2023-08-13 17:36:49 +0530

    Added a Studio Card section under banner.
    Studio cards section will play their corresponding video on hover.

--- src/components/HomePage/HomePage.jsx
+++ src/components/HomePage/HomePage.jsx
@@ -1,10 +1,12 @@
 import BannerSLider from '../BannerSlider/BannerSLider'
+import StudioCards from '../StudioCards/StudioCards'
 import './HomePage.css'
 
 function HomePage() {
   return (
     <div className="HomePage">
         <BannerSLider/>
+        <StudioCards/>
     </div>
   )
 }
---
+++ src/components/StudioCards/StudioCards.jsx
@@ -0,0 +1,40 @@
+import "./StudioCards.css"
+
+function StudioCards() {
+  return (
+    <div className="StudioCards">
+        <div className="StudioCard">
+            <img src="/images/viewers-disney.png" alt="Disney" />
+            <video autoPlay={true} loop={true} muted>
+                <source src="/videos/1564674844-disney.mp4" type="video/mp4"/>
+            </video>
+        </div>
+        <div className="StudioCard">
+            <img src="/images/viewers-pixar.png" alt="Pixar" />
+            <video autoPlay={true} loop={true} muted>
+                <source src="/videos/1564676714-pixar.mp4" type="video/mp4"/>
+            </video>
+        </div>
+        <div className="StudioCard">
+            <img src="/images/viewers-marvel.png" alt="Marvel" />
+            <video autoPlay={true} loop={true} muted>
+                <source src="/videos/1564676115-marvel.mp4" type="video/mp4"/>
+            </video>
+        </div>
+        <div className="StudioCard">
+            <img src="/images/viewers-starwars.png" alt="Star Wars" />
+            <video autoPlay={true} loop={true} muted>
+                <source src="/videos/1608229455-star-wars.mp4" type="video/mp4"/>
+            </video>
+        </div>
+        <div className="StudioCard">
+            <img src="/images/viewers-national.png" alt="National Geographic" />
+            <video autoPlay={true} loop={true} muted>
+                <source src="/videos/1564676296-national-geographic.mp4" type="video/mp4"/>
+            </video>
+        </div>
+    </div>
+  )
+}
+
+export default StudioCards
