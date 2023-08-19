c264556eef3a1852ce7be3191544bdad344c96e4
Roy Joseph <royjoseph.mec@gmail.com>
2023-08-20 02:10:48 +0530

    Added hotstar to studiocards and changed styling.

--- src/components/StudioCards/StudioCards.jsx
+++ src/components/StudioCards/StudioCards.jsx
@@ -33,6 +33,12 @@ function StudioCards() {
                 <source src="/videos/1564676296-national-geographic.mp4" type="video/mp4"/>
             </video>
         </div>
+        <div className="StudioCard hotstar">
+            <img src="/images/hotstarSpec.png" alt="National Geographic" />
+            <video autoPlay={true} loop={true} muted>
+                <source src="https://img10.hotstar.com/video/upload/sources/r1/cms/animations/qc9clm_1669284974594" type="video/mp4"/>
+            </video>
+        </div>
     </div>
   )
 }
